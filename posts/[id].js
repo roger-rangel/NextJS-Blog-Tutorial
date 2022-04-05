4ecd7bdd5420f8c2164b1ac1a576106ac518bf6f
Roger Rangel <roger.rangel.ee@gmail.com>
2022-04-05 09:35:49 +0200

    Implement getStaticPaths

--- posts/[id].js
+++ posts/[id].js
@@ -1,12 +1,16 @@
 import Layout from '../../components/layout'
+import { getAllPostIds } from '../../lib/posts'
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds()
+  return {
+    paths,
+    fallback: false
+  }
+}
 
 export default function Post() {
   return <Layout>...</Layout>
-
-}
-
-export async function getStaticPaths() {
-    // Return a list of possible value for id
 
 }
 

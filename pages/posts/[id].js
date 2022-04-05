cd7fc25d643c7e7ab0b4a12e850707ca9bba45ed
Roger Rangel <roger.rangel.ee@gmail.com>
2022-04-05 09:58:01 +0200

    Fixing a bug caused by version of Next Js

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,17 @@
 import Layout from '../../components/layout'
-import { getAllPostIds } from '../../lib/posts'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  )
+}
 
 export async function getStaticPaths() {
   const paths = getAllPostIds()
@@ -9,14 +21,11 @@ export async function getStaticPaths() {
   }
 }
 
-export default function Post() {
-  return <Layout>...</Layout>
+export async function getStaticProps({ params }) {
+  const postData = getPostData(params.id)
+  return {
+    props: {
+      postData
+    }
+  }
 }
-
-export async function getStaticPaths() {
-    // Return a list of possible value for id
-  }
-
-  export async function getStaticProps({ params }) {
-    // Fetch necessary data for the blog post using params.id
-  }
